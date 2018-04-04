7ff0d4eaf56b819166eee8d239d2fab7f88c7918
Mr.Fake <aviraonepiece@hotmail.com>
2018-04-04 21:16:21 +0800

    优化

--- load.lua
+++ load.lua
@@ -1,39 +1,15 @@
 --waf core load
 require 'config'
 
---Get the client IP
-function get_client_ip()
-    CLIENT_IP = ngx.req.get_headers()["X_real_ip"]
-    if CLIENT_IP == nil then
-        CLIENT_IP = ngx.req.get_headers()["X_Forwarded_For"]
-    end
-    if CLIENT_IP == nil then
-        CLIENT_IP  = ngx.var.remote_addr
-    end
-    if CLIENT_IP == nil then
-        CLIENT_IP  = "unknown"
-    end
-    return CLIENT_IP
-end
-
---Get the client user agent
-function get_user_agent()
-    USER_AGENT = ngx.var.http_user_agent
-    if USER_AGENT == nil then
-       USER_AGENT = "unknown"
-    end
-    return USER_AGENT
-end
-
---Get WAF rule
-function get_rule(rulefilename)
+--Load WAF rule
+function get_rule(rule_file_name)
     local io = require 'io'
-    local RULE_PATH = config_rule_dir
-    local RULE_FILE = io.open(RULE_PATH..'/'..rulefilename,"r")
+    local RULE_PATH = config_rule_dir --将config中的路径赋值
+    local RULE_FILE = io.open(RULE_PATH..'/'..rule_file_name,"r") --只读模式
     if RULE_FILE == nil then
         return
     end
-    RULE_TABLE = {}
+    RULE_TABLE = {}  --声明一个表，把读取的规则放在表里，返回
     for line in RULE_FILE:lines() do
         table.insert(RULE_TABLE,line)
     end
@@ -41,44 +17,70 @@ function get_rule(rulefilename)
     return(RULE_TABLE)
 end
 
---WAF log record for json,(use logstash codec => json)
+--LOad the client IP
+function get_client_ip()
+    CLIENT_IP = ngx.req.get_headers()["X_real_ip"] --获取客户IP的常见做法，先real_ip，再X_forward_for
+    if CLIENT_IP == nil then
+        CLIENT_IP = ngx.req.get_headers()["X_Forwarded_For"]
+    end
+    if CLIENT_IP == nil then
+        CLIENT_IP  = ngx.var.remote_addr
+    end
+    if CLIENT_IP == nil then
+        CLIENT_IP  = ""
+    end
+    return CLIENT_IP
+end
+
+--Load the client user agent --获取UA信息，没有就为空
+function get_user_agent()
+    USER_AGENT = ngx.var.http_user_agent
+    if USER_AGENT == nil then
+       USER_AGENT = ""
+    end
+    return USER_AGENT
+end
+
+
+
+--return logs
 function log_record(method,url,data,ruletag)
-    local cjson = require("cjson")
+    local cjson = require("cjson")  --c语言习惯的json，跨平台
     local io = require 'io'
     local LOG_PATH = config_log_dir
     local CLIENT_IP = get_client_ip()
     local USER_AGENT = get_user_agent()
     local SERVER_NAME = ngx.var.server_name
     local LOCAL_TIME = ngx.localtime()
-    local log_json_obj = {
+    local log_json_obj = {                    --json的logs内容
                  client_ip = CLIENT_IP,
                  local_time = LOCAL_TIME,
                  server_name = SERVER_NAME,
                  user_agent = USER_AGENT,
                  attack_method = method,
-                 req_url = url,
-                 req_data = data,
-                 rule_tag = ruletag,
+                 match_url = url,
+                 match_data = data,
+                 match_rule = ruletag,
               }
-    local LOG_LINE = cjson.encode(log_json_obj)
+    local LOG_RESULT = cjson.encode(log_json_obj)  --格式化为cjson
     local LOG_NAME = LOG_PATH..'/'..ngx.today().."_waf.log"
     local file = io.open(LOG_NAME,"a")
     if file == nil then
         return
     end
-    file:write(LOG_LINE.."\n")
+    file:write(LOG_RESULT.."\n")
     file:flush()
     file:close()
 end
 
---WAF return
+--WAF return  --返回结果
 function waf_output()
     if config_waf_output == "redirect" then
-        ngx.redirect(config_waf_redirect_url, 301)
+        ngx.redirect(config_waf_redirect_url, 301) --301跳转到你的url，我这里用的是后者
     else
         ngx.header.content_type = "text/html"
         ngx.status = ngx.HTTP_FORBIDDEN
-        ngx.say(config_output_html)
+        ngx.say(config_output_html)   --html在config里自己定义
         ngx.exit(ngx.status)
     end
 end
--- process.lua
+++ process.lua
@@ -1,15 +1,15 @@
 require 'fuctions'
 
 function waf_main()
-    if white_ip_check() then
-    elseif black_ip_check() then
-    elseif cc_attack_check() then
-    elseif user_agent_attack_check() then
-    elseif cookie_attack_check() then
-    elseif white_url_check() then
-    elseif url_attack_check() then
-    elseif url_args_attack_check() then
-    elseif post_attack_check() then
+    if white_ip_check() then                       --先检查是不是IP白名单，不是就给后面处理
+    elseif black_ip_check() then                   --再检查是不是黑名单，不是就给后面处理
+    elseif cc_attack_check() then                  --判断是不是cc攻击，不是就给后面处理
+    elseif user_agent_attack_check() then           --判断UA有没有不干净的东西，没有就给后面处理
+    elseif cookie_attack_check() then               --判断cookie有没有不干净的，没有就给后面处理
+    elseif white_url_check() then                   --看看符不符合白名url，不符合接着匹配
+    elseif url_attack_check() then                   --看看有没有不该让访问的目录，没有就接着匹配
+    elseif url_args_attack_check() then            --看看get请求的参数是不是干净的，干净的话接着匹配
+    elseif post_attack_check() then              --最后看看post的内容有没有恶意请求
     else
         return
     end
